02005467a06ccb88052a45fd8b266cad600cc587
lalala <lbondyaaa@gmail.com>
2024-12-09 23:37:56 +0300

    version 0.0.2

--- split _аlat_аile.py
+++ split _аlat_аile.py
@@ -16,13 +16,17 @@ lines = clipboard_data.splitlines()
 current_file = None
 current_content = []
 
+# Список файлов для исключения
+excluded_files = ["harkach_markup_converter.py"]
+
 for line in lines:
     # Проверка на начало нового файла
     if line.startswith("===") and line.endswith("==="):
         # Сохранение текущего файла, если есть
         if current_file and current_content:
-            with open(os.path.join(output_dir, current_file), "w", encoding="utf-8") as out_file:
-                out_file.writelines(current_content)
+            if current_file not in excluded_files:
+                with open(os.path.join(output_dir, current_file), "w", encoding="utf-8") as out_file:
+                    out_file.writelines(current_content)
 
         # Установка имени нового файла (без добавления ".py" к имени, если оно уже есть)
         current_file_name = line.strip("= ").replace(" ", "_").lower()
@@ -37,7 +41,8 @@ for line in lines:
 
 # Сохранение последнего файла
 if current_file and current_content:
-    with open(os.path.join(output_dir, current_file), "w", encoding="utf-8") as out_file:
-        out_file.writelines(current_content)
+    if current_file not in excluded_files:
+        with open(os.path.join(output_dir, current_file), "w", encoding="utf-8") as out_file:
+            out_file.writelines(current_content)
 
 print(f"Файлы успешно разделены и сохранены в папке '{output_dir}'")
